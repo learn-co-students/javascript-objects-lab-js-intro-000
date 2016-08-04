69db740b35a760e7b85b6bb9ea92a1a11d0e0843
Tsi Lung <tsi.lung@gmail.com>
2016-08-04 17:00:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-recipes = {};
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},object,{key:value});  
+  return Object.assign({},object,{[key]:value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  obj = Object.assign({},object);
+  var obj = Object.assign({},object);
   delete obj[key];
   return obj;
 }
