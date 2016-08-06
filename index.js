12e02c0367798290b66f7dda044689e1f31db546
B F <b.fizzle@gmail.com>
2016-08-06 23:14:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {};
+
+var updateObjectWithKeyAndValue = (object, key, value) => Object.assign({}, object, {[key]: value})
+
+var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  object[key] = value;
+  return object;
+}
+
+var deleteFromObjectByKey = (object, key) => {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+var destructivelyDeleteFromObjectByKey = (object, key) => {
+  delete object[key];
+  return object;
+}
