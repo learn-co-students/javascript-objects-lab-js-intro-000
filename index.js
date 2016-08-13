111ab5838fd26041e50da1cf9ca25a7c885b1d88
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-13 17:07:09 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object, key, value);
   return newObj;
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+};
+
+function deleteFromObjectByKey(object, key){
+
 }
