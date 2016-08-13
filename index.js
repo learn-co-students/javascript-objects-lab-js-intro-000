eb3e5eddbc37936f9a1670fd6c8cf7e2da171d85
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-13 16:58:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var recipes = {
   'recipe 1': "This is recipe 1",
-  'recipe 1': "This is recipe 1",
-  'recipe 1': "This is recipe 1",
-  'recipe 1': "This is recipe 1"
+  'recipe 2': "This is recipe 2",
+  'recipe 3': "This is recipe 3",
+  'recipe 4': "This is recipe 4"
 };
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object, key, value);
+  return newObj;
+}
