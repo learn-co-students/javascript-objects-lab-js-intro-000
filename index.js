d655cde130704b8e390b6a2bf477f36147c0e533
maxgrok <maxgrok2011@gmail.com>
2016-08-15 11:57:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var recipes ={}
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
+  var newObj = Object.assign({},object,{[key]:value});
+  return newObj;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
 function deleteFromObjectByKey(object, key){
-  delete object.key;
-  return object;
+  var newObj = Object.assign({}, object)
+  newObj // { foo: 'bar' }
+  delete newObj.key
+  return newObj;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object.key;
