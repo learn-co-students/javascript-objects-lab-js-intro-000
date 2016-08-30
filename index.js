e80a22ce5ba38fd9a68b028ecfe721db97505494
JinOk McElwain <jjm2140@gmail.com>
2016-08-30 00:53:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 var recipes = {}
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign ({}, object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey (object, key) {
+  //non-destructively deletes 'key' from a clone of object and returns new object
+  return Object.assign({}, object)
+}
+
+function destructivelyDeleteFromObjectByKey (object, key) {
+  delete object.key;
+  return object
+}
