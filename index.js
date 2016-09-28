5cd8a3fc182514c1845cb3771a2fa38e1a083eb7
Shalom Ayidu <malohs@gmail.com>
2016-09-28 10:23:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {soup:'salt'};
+
+function updateObjectWithKeyAndValue(object, key, value){
+    return Object.assign({},object,{[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+    object[key] = value;
+    return object;
+}
+
+function deleteFromObjectByKey(object, key){
+   var newobj =  Object.assign({},object);
+   delete newobj[key];
+   return newobj;
+}
+
+
+function destructivelyDeleteFromObjectByKey(object,key){
+   delete object.key;
+   return object;
+}
