a642a8d2f7e097bd8228b31ac7f4448cacaa23c6
kwebster2 <kevin.r.webster@gmail.com>
2016-10-21 08:09:39 +0530

    done

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object[key] = value
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var clone = Object.assign({}, object)
+  delete clone[key]
+  return clone
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
