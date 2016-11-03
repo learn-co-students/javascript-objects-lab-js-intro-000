6c72c0f836060d2122b4c5ab95abf53cd98837f0
nikymorg <nicole.morgan@gmail.com>
2016-11-03 18:45:25 -0400

    Redone

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-var recipes = { foo: 'bar' };
+var recipes = { foo: 'bar' }
 
-
-function updateObjectWithKeyAndValue(object, key, value) {
-    return Object.assign({}, object, { [key]: value })
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  return Object.assign(object, {[key]: value})
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
+  delete object.key
+  return object
 }
