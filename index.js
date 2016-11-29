72f23ab2e7d7a9b0198c307d2ed0be2b87e09254
Stuart Hahn <slumpcity@outlook.com>
2016-11-29 04:01:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { [key]:value });
+  const newObj = Object.assign({}, object, {[key]: value})
+  return newObj
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
+  object[key] = value
   return object
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj;
+  const newObj = Object.assign({}, object)
+
+  delete object.key
+  return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
-  return object;
+
+  return object
 }
