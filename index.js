69bd4475eb98831e32e9bbc0ef6e2b0d3b3da6b3
Usman Pervaiz <de.usman@yahoo.com>
2017-01-16 23:53:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = { 
+egg : 1,
+butter : '1/2 cup',
+oil : '1/4 cup'
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+var newObj = Object.assign({}, object, {[key] : value});
+return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key] : value});
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+delete object[key];
+return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key]
+  var newObj = Object.assign({}, object)
+  return newObj
+}
