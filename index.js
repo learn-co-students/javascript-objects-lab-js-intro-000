61db889c8e287576b871f418200db426d728d22d
Abraham  Kwak <kwak.abe@gmail.com>
2017-01-17 18:53:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = {
+  eggs: 3,
+  cheese: "2 slices",
+  salt: "1 tbsp",
+  pepper: "1 tbsp"
+};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
