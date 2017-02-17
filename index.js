cd932dff4d776c6cb1450b4f1dc08ee5acf9d2b3
Joshua Jenkins <joshjenkins6@yahoo.com>
2017-02-17 23:14:18 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 // Objects updateObjectWithKeyAndValue(object, key, value)
 // returns a clone of `object` by adding `key` and `value` (it is non-destructive):
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+
+  return Object.assign({}, object, { [key]: value });
 }
 
 // Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object`
@@ -20,8 +20,12 @@ return object;
 // Objects deleteFromObjectByKey(object, key) deletes `key`
 // from a clone of object and returns the new object (it is non-destructive):
 function deleteFromObjectByKey(object, key) {
-delete object.key;
-return object;
+
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key];
+
+  return newObj;
 
 }
 
