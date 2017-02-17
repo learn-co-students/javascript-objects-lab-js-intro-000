ce1e371e6ebc05f3b82a3464c55115e1881ed3a7
Joshua Jenkins <joshjenkins6@yahoo.com>
2017-02-17 23:10:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+/*Objects defines a recipes object*/
+ var recipes = {};
+
+// Objects updateObjectWithKeyAndValue(object, key, value)
+// returns a clone of `object` by adding `key` and `value` (it is non-destructive):
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+// Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object`
+// with the given `key` and `value` (it is destructive):
+function destructivelyUpdateObjectWithKeyAndValue(object,key, value) {
+ object[key] = value;
+
+return object;
+
+}
+
+// Objects deleteFromObjectByKey(object, key) deletes `key`
+// from a clone of object and returns the new object (it is non-destructive):
+function deleteFromObjectByKey(object, key) {
+delete object.key;
+return object;
+
+}
+
+
+// Objects destructivelyDeleteFromObjectByKey(object, key) deletes `key` from
+// object and returns object:
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
