d4a48803ef2b030c2f29b219a6effb4b92297158
Tommy Mitchell <mitchell.tommy@gmail.com>
2017-03-26 20:07:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value){
+object[key]=value
+return object
+}
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value
+  return object
+}
