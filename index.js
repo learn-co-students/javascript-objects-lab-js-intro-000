fbf217a8e5387213121d686ae24ce46a572297c7
Tommy Mitchell <mitchell.tommy@gmail.com>
2017-03-26 20:22:12 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value
   return object
 }
+function deleteFromObjectByKey(object, key){
+  delete object.key
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object)
+  delete newObj[key]
+  return newObj
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
