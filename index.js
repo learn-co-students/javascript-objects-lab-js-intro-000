41c02466014aeed95118b06708f2a7fca891df0a
Nishu Gupta <nishug001@gmail.com>
2017-04-02 05:01:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-recipes = {}
+  var recipes = {}
 
-
+  function updateObjectWithKeyAndValue(object,key,value){
+    var obj_new = Object.assign({},object)
+    obj_new[key] = value
+    return obj_new
+  }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object[key] = value
