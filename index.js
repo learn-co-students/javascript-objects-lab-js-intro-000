89d95bcfe7f4c29a756e8441a9acfed52e472cab
Nazim Turdiyev <nazik_111@mail.ru>
2017-04-30 17:15:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+    var clone = Object.assign(recipes,object,{[key]:value});
+    return clone;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key,
+ value) {
+   return Object.assign(object,{[key]:value});
+ }
+
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({},object);
+  delete clone.key;
+  return clone;
+}
+
