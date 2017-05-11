9784a650ae29652adab527d2c02771997a473064
Margaret Hoagland <mehoagland@gmail.com>
2017-05-11 17:12:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {
+  eggs: 3,
+};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  //object[key] = value;
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var obj2 = Object.assign({}, object, [key]);
+  delete obj2[key];
+  return obj2;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
