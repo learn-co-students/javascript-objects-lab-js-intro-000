aff52721e799d98b260e96fb8c668a418aa8be54
L North <lnordell@gmail.com>
2017-08-16 15:45:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var recipes = {};
+var recipes = new Object();
 
-function updateObjectWithKeyAndValue(obj, key, val){
-  return Object.assign({}, obj, {[key]: val});
+function updateObjectWithKeyAndValue (obj, key, val) {
+  return Object.assign({}, obj, { [key]: val });
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, val){
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, val) {
   obj[key] = val;
-  // can't use obj.key = value. not sure why. assigning it to value?
   return obj;
 }
 
-function destructivelyDeleteFromObjectByKey(obj, key){
-  delete obj.key;
-  // or delete obj[key];
-  return obj;
-}
-
-function deleteFromObjectByKey(obj, key) {
+function deleteFromObjectByKey (obj, key) {
   var newObj = Object.assign({}, obj);
-  delete newObj.key;
-  // or delete obj[key];
+  delete newObj[key];
   return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey (obj, key) {
+  delete obj[key];
+  return obj;
 }
