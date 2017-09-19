44ae42405d362f48767867a0a1c62d939140d31c
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-19 21:40:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = { flour: '3 cups'}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  // object[key] = value
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  return delete Object.assign({}, object, key)
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
