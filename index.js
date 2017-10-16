78910c9d8d0e824b3bf220c7e48209781dd9ef0a
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-16 12:23:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = { eggs: 2}
+var recipes = { eggs: 2 }
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
@@ -9,4 +9,20 @@ function updateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object)
   newObject[key] = value
   return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
