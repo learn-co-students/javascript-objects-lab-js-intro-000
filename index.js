745e071d99095d401e22892e983195323a81dc26
Lou Angeles <lou.c.angeles@gmail.com>
2018-01-15 22:03:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-var recipes = new Object();
+var recipes = {}
 
-updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
+
   return object
 }
 
-updateObjectWithKeyAndValue('recipes', 'Shrimp Egg Foo Young', 'Classic');
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
+}
