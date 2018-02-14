789e2740a047eba285a19865f4446e2c97e3ea2e
Alex Segers <a.segers.dev@gmail.com>
2018-02-14 02:47:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function Objects() {
+  return recipes = new Object()
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
