7755aadae5e981c41462b01db2ac7c8a22f99e1d
vijay khatri <vijaykhatri22@gmail.com>
2018-03-07 19:08:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes ={};
+
+function updateObjectWithKeyAndValue(recipes,key,value){
+  return Object.assign({},recipes,{[key] : value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes,key,value){
+  recipes[key]=value;
+  return recipes;
+}
+function deleteFromObjectByKey(recipes,key){
+  var temp = Object.assign({},recipes);
+  delete temp[key];
+  return temp;
+}
+
+function destructivelyDeleteFromObjectByKey(recipes,key){
+  delete recipes[key];
+  return recipes;
+}
