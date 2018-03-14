0d5b76880fbbbbb5a30e799ca63394388e90f072
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-14 18:45:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes={breakFast:"Cereal"};
+
+function updateObjectWithKeyAndValue(obj, key, value){
+   return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+   obj[key]=value;
+   return obj
+}
+
+function deleteFromObjectByKey(obj, key){
+
+    return delete Object.assign({}, obj, {})
+
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+   delete obj[key];
+   return obj
+}
