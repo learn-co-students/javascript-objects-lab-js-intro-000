6f3ae5e7084214273d7a8bd6beba3be43b42310d
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-02 17:04:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {eggs: '2', bagel: '1', creeamCheese: '2spreads'};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newRecipes = Object.assign ({}, object, {[key]: value});
+    
+    return newRecipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+    return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newRecipes = Object.assign({}, object);
+  delete newRecipes[key];
+    return newRecipes;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+    return object;
+}
