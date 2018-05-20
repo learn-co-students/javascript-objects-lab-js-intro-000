4d8df835ebdb46e3553836b99a6084f58eab0bd1
William Adu-Krow <mubah125@gmail.com>
2018-05-20 19:51:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var recipes = {};
+
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+object[key] = value;
+
+return object;
+};
+
+function deleteFromObjectByKey(object, key){
+
+var newObj = Object.assign({}, object);
+
+return delete newObj[key];
+
+};
+
+function destructivelyDeleteFromObjectByKey(object , key) {
+
+delete object[key];
+
+return object;
+};
