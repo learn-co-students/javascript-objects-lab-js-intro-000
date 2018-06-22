04253ff133c5f252b006684f54cd48bcb723248e
eggsvonsatan <elbadry.d@gmail.com>
2018-06-22 02:32:18 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value) {
   recipes[key] = value; return recipes
 }
+
 function deleteFromObjectByKey (object, key) {
-  delete recipes.key; return recipes
+  var newRecipes = Object.assign({}, object);
+  delete newRecipes[key];
+  return newRecipes
 }
+
 function destructivelyDeleteFromObjectByKey(recipes, key){
  delete recipes[key];
  return recipes}
