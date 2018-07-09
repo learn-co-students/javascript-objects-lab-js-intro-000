bd001ba33311f0bbd51f917e2d83a85c34c4db2d
Jason Viger <jason.viger@gmail.com>
2018-07-09 13:02:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {};
+
+var updateObjectWithKeyAndValue = function(object,key,value){
+  var newObject = Object.assign({},object);
+  newObject[key] = value;
+  return newObject;
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object,key,value){
+  
+  object[key] = value;
+  return object;
+}
+var deleteFromObjectByKey = function(object,key){
+  
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+
+var destructivelyDeleteFromObjectByKey = function(object,key){
+  
+  delete object[key];
+  return object;
+}
