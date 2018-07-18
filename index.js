47a8232dc6a5b867dcd2cc24be47ef293c83f14a
Kiley Bobbitt <jkileybob@aol.com>
2018-07-18 19:13:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 var recipes = {
-  bananaDaiquiri: "rum" "sugar" "lime" "banana" "ice"
+  bananaDaiquiri: "rum, sugar syrup, lime juice, banana, ice"
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newRecipe = Object.assign ({}, object)
+  newRecipe
+  delete newRecipe.key
+  return newRecipe
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
 }
