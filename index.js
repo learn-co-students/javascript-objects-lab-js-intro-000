56b20cde22bae37df1eee760253b9cc35dd12833
Annafin Murloc <anna.y.tang@gmail.com>
2018-08-05 17:00:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  // returns an object with the orignal key value pairs and the new key value pair
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  // updates `object` with the given `key` and `value` (it is destructive) and returns
+  // the entire updated object
+  object[key] = value
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  // deletes `key` from a clone of object and returns the new object
+  // (it is non-destructive)
+  var clone = Object.assign({}, object)
+  delete clone[key]
+  return clone;
+  return object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  // returns object without the delete key/value pair
+  delete object[key];
+  return object;
+}
