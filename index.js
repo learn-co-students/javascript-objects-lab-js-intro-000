94e8d19c45c5f0f65fb76e1c84ea8066ec354a57
andy reyes <11710reyes@gmail.com>
2018-11-14 20:44:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = { eggs:'2'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes.flour = '2 cups'
+  return recipes
+}
+updateObjectWithKeyAndValue(object, flour, '2 cups')
+
