d83bd4be1c9921615167a946e532c013dba2ede8
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-05 03:29:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+let recipes = {
+  prop: 1
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  let newObj = Object.assign({},object)
+  newObj[key] = value
+  return newObj
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  let newObj = Object.assign({},object)
+  delete newObj[key]
+  return newObj
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
