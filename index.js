078456e95a369410b780914597da468e67bedd5a
Tara Wright <wrightt2015@gmail.com>
2019-06-05 23:30:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var recipes = {
-
+  'pasta': "Chicken Alfredo",
+  seafood: "Lobster",
+  salad: "Caesar Salad"
 };
+
+
+function updateObjectWithKeyAndValue (object, key, value){
+  Object.assign({}, object, key, value);
+  return recipes;
+}
