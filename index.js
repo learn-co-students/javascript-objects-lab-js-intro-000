2bc2923819788178fca8637965c81726473987fb
Tara Wright <wrightt2015@gmail.com>
2019-06-07 02:21:33 +0000

    Done.

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key];
   return newObj;
 }
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
