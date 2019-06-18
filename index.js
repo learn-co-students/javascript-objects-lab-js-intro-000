82643c8038535722518165e985bcff3b00388d40
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 19:14:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {};
+
+var updateObjectWithKeyAndValue = function(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  return Object.assign(object, {[key]: value})
+}
+
+var deleteFromObjectByKey = function(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+var destructivelyDeleteFromObjectByKey = function(object, key){
+  delete object[key]
+  return object
+}
