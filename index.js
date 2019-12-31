b3331b75e23e5cda6e423c8f8f37b93d3d31e0ff
voluble-block-7446 <iagogomes80@gmail.com>
2019-12-31 00:43:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function updateObjectWithKeyAndValue(object, key, value){
-  
+var obj = { prop: 1 };
+function updateObjectWithKeyAndValue(obj, key, value){
+  var ob=Object.assign({}, obj);
+  ob[key]=value;
+  return ob;
+}
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key]=value;
+  return obj;
+}
+function deleteFromObjectByKey(obj, key){
+  var ob=Object.assign({}, obj);
+  delete ob[key];
+  return ob;
+}
+function destructivelyDeleteFromObjectByKey(obj, key){
+  delete obj[key];
+  return obj;
 }
