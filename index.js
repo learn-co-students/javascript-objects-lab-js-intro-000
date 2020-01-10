80b05b2d8e380e1401de23a4cb65e89749398be5
Brett Schlesinger <brettgs@gmail.com>
2020-01-10 03:37:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+var recipes = {
+  flour: "white",
+  sugar: "brown",
+  eggs: "yolks"
+}
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
