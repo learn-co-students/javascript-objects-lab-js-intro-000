196d3b99ab35e580f862e45c0d2430885dd4b360
enid svymbersky <enid.bersky@gmail.com>
2020-01-11 23:59:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function updateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
+}
