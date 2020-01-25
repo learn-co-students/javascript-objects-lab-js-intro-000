0a55167a11f19d2f0e6573a09c9033c246c2c3e0
Scott Keplinger <skeplin17@gmail.com>
2020-01-25 00:08:43 +0000

    Done.

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function deleteFromObjectByKey (object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey (object, key) {
-  var object = delete object.key;
+  var object = delete object[key];
    return object;
 }
