f0c919332d9e5bd7974abece4dfb08a4eb297a46
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-01 18:36:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-var recipes = new Object();
+var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key]=value;
-return object;
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value
+  return object
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object,{[key], value});
+return Object.assign({}, object,{[key]: value});
 }
-
 
 function deleteFromObjectByKey(object, key){
   var newly =  Object.assign({},object)
-  delete newly[key]
-  return newly
+  delete newly[key];
+  return newly;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key]
-  return object
+  delete object[key];
+  return object;
 }
