d8a9fd160f75fbbea8a00f4fd53179b86283cbe2
Morgan Matney <morgan.matney89@gmail.com>
2020-04-12 00:48:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var obj = {key: 'value'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({},key)
+  delete newObj.key;
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key];
+}
