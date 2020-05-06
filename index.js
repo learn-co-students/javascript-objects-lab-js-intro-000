9c87e59818182397ddd0edaa84990a8559196740
T Nicole <tnp421@yahoo.com>
2020-05-06 02:55:20 +0000

    Done.

--- index.js
+++ index.js
@@ -27,5 +27,6 @@ function deleteFromObjectByKey(object, key) {
 //destructivelyDeleteFromObjectByKey(object, key)
 //returns object without the delete key/value pair:
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete.key
+  return delete object[key]
+
 }
