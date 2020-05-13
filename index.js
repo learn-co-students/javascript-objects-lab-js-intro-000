922576dc6326a290e22cb02bc1e9939f336eeb08
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 11:17:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes={};
+function updateObjectWithKeyAndValue(obj, key, value){
+return Object.assign({},obj,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+ obj[key]=value
+ return obj
+ }
+
+ function deleteFromObjectByKey(obj,key){
+   var newobj=Object.assign({},obj)
+   delete newobj[key]
+   return newobj
+    }
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+  }
