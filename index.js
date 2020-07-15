726b0bee8ed9339570792b614453e1c61290afc1
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-15 03:47:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  return delete newObj[key] ? newObj : pass;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {return delete object[key] ? object : pass;}
