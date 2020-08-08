43625e13c59697bde65eadd4a42ff3215b719595
Corbin Hall <hallcorbin92@gmail.com>
2020-08-08 03:44:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value})
+}
+
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, object[key])
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
