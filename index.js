98bc86c20d793404269d9b4548e0283fa1e8f875
Ada Huang <ada10huang@gmail.com>
2020-08-11 23:47:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var updateObjectWithKeyAndValue = (object, key, value) => {
-  return Object.assign({}, object, {[key], value})
+  return Object.assign({}, object, {[key]: value})
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  object[key] = value;
+  return object
+}
+
+var deleteFromObjectByKey = (object, key) => {
+  let object = Object.assign({}, object);
+  delete object[key];
+  return object
 }
