272468cb83a6d0c2d30237a439933524bc0aad09
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-27 00:50:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function updateObjectWithKeyAndValue(object, key, value){
-  object[`${key}`] = value;
+  return (Object.assign({},object,{ [key] : `${value}` }))
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return (Object.assign(object, { [key] : `${value}`}))
+}
+
+function deleteFromObjectByKey(object, key){
+  var newobject = Object.assign({},object)
+  delete newobject[key]
+  return (newobject)
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  return delete object[key]
 }
