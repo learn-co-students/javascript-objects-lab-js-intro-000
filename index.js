d45a4839f9e4e2c6d7b36f12b1eccbb5e942807c
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-04 17:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-var recipes = {
+var recipes {
   prop: 1
 }
-
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object {[key]: value})
+}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
-  recipes[key] = value
-  return recipes
-}
-
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
-  return newObj
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-  return object
 }
