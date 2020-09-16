e8b88e99daed044d42d591633bca95a883c16204
Nayeli C <nayelicruz12@gmail.com>
2020-09-16 22:15:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var recipes = {
+  apples: 6,
+  nutmeg: '1/4 tsp'
+};
+
+function updateObjectWithKeyAndValue (obj, key, value) {
+  return Object.assign ({}, obj, { [key]: value })
+}
+
+ updateObjectWithKeyAndValue(recipes, 'cinammon', '1/4 tsp')
+
+ recipes
+
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value
+
+  return recipes
+}
+
+
+var newRecipes = Object.assign({}, recipes)
+
+function deleteFromObjectByKey(recipes, key) {
+  delete newRecipes.key
+
+  return newRecipes
+}
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete recipes.key
+
+  return recipes
+}
