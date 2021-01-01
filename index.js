e1c2a1e56df6b8869bc192039a0b3beadc205891
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-01 18:26:10 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+var updateObjectWithKeyAndValue = (object, key, value) => {
+    return Object.assign({}, object, {[key]: value})
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+    object[key] = value
+   return object
+}
+
+var deleteFromObjectByKey = (object, key) => {
+    const newObj = Object.assign({}, object)
+    delete newObj[key]
+    return newObj
+}
+
+var destructivelyDeleteFromObjectByKey = (object, key) => {
+    delete object[key]
+   return object
+}
