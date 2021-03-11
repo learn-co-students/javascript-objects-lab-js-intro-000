014444eaa66e1022809ef15720008faf082c01a6
Sophie Shin <sophie@alloy.co>
2021-03-11 20:17:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {mexican: 'tacos'}
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign({}, object, {[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  var object1 = Object.assign({}, object)
+    delete object1[key]
+    return object1
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
